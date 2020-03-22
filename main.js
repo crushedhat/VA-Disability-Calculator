ad3c103dbd35fa7a9d41a40775ed2c8ddce10c74
Joe Mora <morajp3@gmail.com>
2020-03-22 11:38:01 -0400

    added output of combined and complete disability rating to text box

--- main.js
+++ main.js
@@ -31,14 +31,13 @@ function calculatePercentage(rating) {
     arr.sort(function(first, last){
         return last-first
     });
-    //console.log(arr)
     combinedRating = arr[0]+(100-arr[0])*(arr[1]/100)
     
     for(let i = 2; i < arr.length; i++) {
     totalRating = combinedRating+(100-combinedRating)*(arr[i]/100)
     combinedRating = Math.round(totalRating)
     }
-    console.log(combinedRating)
+    document.getElementById("cdisability").value = Math.round(combinedRating)
+    document.getElementById("fdisability").value = Math.round(combinedRating/10) * 10
     //return highest disbilited into a combined % function combinedDis + (100-combined) * (new claim percentage/100)
-    //create checkbox for bilateral condition to facter that in another function
 }
